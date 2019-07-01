4fb56ca120867d1526790e00a7adc8d4c274a87f
Phạm Thanh Tùng <thanhtung.uet@gmail.com>
2019-07-01 18:37:53 +0700

    Chưa xong @@ - Tree select

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './_pages/login/login.component';
-import { PageComponent } from './_pages/page/page.component';
-import { AuthGuard } from './_helpers';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './_pages/login/login.component';
+import {PageComponent} from './_pages/page/page.component';
+import {AuthGuard} from './_helpers';
+import {TreeComponent} from './_shared/components/tree/tree.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '', component: PageComponent, children:[
-    {path: 'home', loadChildren: () => import('./_modules/home/home.module').then(m => m.HomeModule)},
-    {path: 'admin', loadChildren: () => import('./_modules/admin/admin.module').then(m => m.AdminModule)}
-  ], canActivate: [AuthGuard]},
-  {path: '**', redirectTo: 'login', pathMatch: 'full'}
+  {
+    path: 'tree',
+    component: TreeComponent,
+  },
+  {
+    path: '', component: PageComponent, children: [
+      {path: 'home', loadChildren: () => import('./_modules/home/home.module').then(m => m.HomeModule)},
+      {path: 'admin', loadChildren: () => import('./_modules/admin/admin.module').then(m => m.AdminModule)}
+    ], canActivate: [AuthGuard]
+  },
+  {path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { ToastrModule } from 'ngx-toastr';
-import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from './app-routing.module';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CollapseModule} from 'ngx-bootstrap/collapse';
+import {ToastrModule} from 'ngx-toastr';
+import {CommonModule} from '@angular/common';
+import {AppRoutingModule} from './app-routing.module';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
-import { AppComponent } from './app.component';
-import { PageComponent } from './_pages/page/page.component';
-import { LoginComponent } from './_pages/login/login.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
-import { SidebarComponent } from './_pages/page/sidebar/sidebar.component';
-import { NavbarComponent } from './_pages/page/navbar/navbar.component';
-import { FooterComponent } from './_pages/page/footer/footer.component';
-import { SpinnerComponent } from './_shared/components/spinner/spinner.component';
+import {TreeModule} from 'primeng/tree';
 
+import {AppComponent} from './app.component';
+import {PageComponent} from './_pages/page/page.component';
+import {LoginComponent} from './_pages/login/login.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ErrorInterceptor, JwtInterceptor} from './_helpers';
+import {SidebarComponent} from './_pages/page/sidebar/sidebar.component';
+import {NavbarComponent} from './_pages/page/navbar/navbar.component';
+import {FooterComponent} from './_pages/page/footer/footer.component';
+import {SpinnerComponent} from './_shared/components/spinner/spinner.component';
+import {TreeComponent} from './_shared/components/tree/tree.component';
+import {ClickOutsideModule} from 'ng-click-outside';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,8 @@ import { SpinnerComponent } from './_shared/components/spinner/spinner.component
     SidebarComponent,
     NavbarComponent,
     FooterComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    TreeComponent
   ],
   imports: [
     BrowserModule,
@@ -38,12 +42,16 @@ import { SpinnerComponent } from './_shared/components/spinner/spinner.component
     CollapseModule.forRoot(),
     ToastrModule.forRoot(),
     HttpClientModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    TreeModule,
+    BrowserAnimationsModule,
+    ClickOutsideModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
