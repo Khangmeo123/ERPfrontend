91ac717847a35335c94ec86c31b81e4bc6a7b45e
huongnt <huongnguyenhd96@gmail.com>
2019-07-06 10:33:18 +0700

    import them module

--- src/app/_modules/admin/admin.module.ts
+++ src/app/_modules/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
-import { MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorModule } from 'src/app/_shared/components/error/error.module';
 import { AdminComponent } from './admin.component';
@@ -18,7 +18,8 @@ import { UserComponent } from './_pages/user/user.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    ErrorModule
+    ErrorModule,
+    MatIconModule
   ]
 })
 export class AdminModule { }
