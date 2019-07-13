72ca59efb35070b3b8d623566fd7f63404e7787e
LeAnh <leanh2194@gmail.com>
2019-07-13 18:05:39 +0700

    fix error

--- src/app/_shared/components/error/error.module.ts
+++ src/app/_shared/components/error/error.module.ts
@@ -1,15 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TooltipModule} from 'ngx-bootstrap/tooltip';
-import {ErrorComponent} from './error.component';
-import {TranslateModule} from '@ngx-translate/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ErrorComponent } from './error.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ErrorComponent],
   imports: [
     CommonModule,
     TooltipModule.forRoot(),
-    TranslateModule,
+    TranslateModule.forRoot(),
   ],
   exports: [
     ErrorComponent,
