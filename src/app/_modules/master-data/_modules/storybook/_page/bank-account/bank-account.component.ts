fc9c6028825afcc75e5103e66ff1de98a8854670
tungpt <thanhtung.uet@gmail.com>
2019-07-13 09:16:32 +0700

    3 màn hình: - Tài khoản ngân hàng - Template biểu thuế - Hệ thống tài khoản

--- src/app/_modules/master-data/_modules/storybook/_page/bank-account/bank-account.component.ts
+++ src/app/_modules/master-data/_modules/storybook/_page/bank-account/bank-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationModel } from '../../../../../../_shared/modules/pagination/pagination.model';
 
 @Component({
   selector: 'app-bank-account',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankAccountComponent implements OnInit {
 
+  bankAccountsModal = false;
+
+  pagination = new PaginationModel();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleBankAccountsModal() {
+    this.bankAccountsModal = !this.bankAccountsModal;
+  }
+
+  paginationOutput(event) {
+
+  }
 }
