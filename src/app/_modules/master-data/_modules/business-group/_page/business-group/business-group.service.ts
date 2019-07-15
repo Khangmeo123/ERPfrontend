a7f7e305f0033334e04439c91bba1e9c7f5b19c3
LeAnh <leanh2194@gmail.com>
2019-07-15 18:47:20 +0700

    add business-group.service + change environtment

--- src/app/_modules/master-data/_modules/business-group/_page/business-group/business-group.service.ts
+++ src/app/_modules/master-data/_modules/business-group/_page/business-group/business-group.service.ts
@@ -1,4 +1,39 @@
-export class BusinessGroupService {
+import { BusinessGroupEntity } from './../../../../_backend/business-group/business-group.entity';
+import { BusinessGroupRepository } from './business-group.repository';
+import { BusinessGroupSearchEntity } from './../../../../_backend/business-group/business-group.searchentity';
+import { BusinessGroupForm } from './../../../../_backend/business-group/business-group.form';
+import { BehaviorSubject } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
-  constructor() { }
+export class BusinessGroupService {
+  public listBusinessGroups: BehaviorSubject<BusinessGroupEntity[]> = new BehaviorSubject([]);
+  constructor(private fb: FormBuilder, private businessGroupRepository: BusinessGroupRepository) { }
+
+  getList(businessGroupSearchEntity: BusinessGroupSearchEntity) {
+    this.businessGroupRepository.getList(businessGroupSearchEntity).subscribe(res => {
+      if (res) {
+        this.listBusinessGroups.next(res);
+      }
+    }, err => {
+      if (err) {
+
+      }
+    });
+  }
+
+  getId(businessGroupId: string) {
+
+  }
+
+  add(businessGroupEntity: any) {
+
+  }
+
+  edit(businessGroupEntity: any) {
+
+  }
+
+  delete(businessGroupId: string) {
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  production: false,
   apiUrlApps: 'https://localhost:5001/api/apps/',
 };
 
