d93d76f0008dd5167c77da8ad26f1ecd0712bec6
Leanh1011 <leanh2194@gmail.com>
2019-07-15 18:53:39 +0700

    merge

--- src/app/_modules/master-data/_backend/business-group/business-group.entity.ts
+++ src/app/_modules/master-data/_backend/business-group/business-group.entity.ts
@@ -1,17 +1,6 @@
 import { Entity } from 'src/app/_helpers/entity';
 
 export class BusinessGroupEntity extends Entity {
-<<<<<<< HEAD
-
-    code: String;
-    name: String;
-    description: string;
-
-    constructor(businessGroupEntity?: any){
-        super(businessGroupEntity);
-    }
-
-=======
     name: string;
     code: string;
     description: string;
@@ -19,5 +8,4 @@ export class BusinessGroupEntity extends Entity {
     constructor(businessGroupEntity?: any) {
         super(businessGroupEntity);
     }
->>>>>>> a7f7e305f0033334e04439c91bba1e9c7f5b19c3
 }
--- src/app/_modules/master-data/_backend/business-group/business-group.form.ts
+++ src/app/_modules/master-data/_backend/business-group/business-group.form.ts
@@ -1,14 +1,4 @@
 import { FormModel } from './../../../../_helpers/form-model';
-<<<<<<< HEAD
-import { FormControl, Validators, FormGroup, AbstractControl } from '@angular/forms';
-import { BusinessGroupEntity } from 'src/app/_modules/master-data/_backend/business-group/business-group.entity';
-import { requiredField, checkLength } from 'src/app/_helpers';
-
-export class BusinessGroupForm extends FormModel{
-    code = new FormControl('',[requiredField]);
-    name = new FormControl('',[requiredField]);
-    description =  new FormControl('');
-=======
 import { FormControl, Validators, FormGroup, AbstractControl, FormArray } from '@angular/forms';
 import { requiredField, checkLength } from 'src/app/_helpers';
 import { BusinessGroupEntity } from './business-group.entity';
@@ -18,7 +8,6 @@ export class BusinessGroupForm extends FormModel {
     name = new FormControl('', [requiredField]);
     code = new FormControl('', [requiredField]);
     description = new FormControl('');
->>>>>>> a7f7e305f0033334e04439c91bba1e9c7f5b19c3
 
     constructor(businessGroupEntity?: BusinessGroupEntity) {
         super(businessGroupEntity);
