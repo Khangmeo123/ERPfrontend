a8fc27fa764e9fbfdfbc9c17dede21c375a78e10
tungpt <thanhtung.uet@gmail.com>
2019-07-16 14:48:28 +0700

    Close picker on enter

--- src/app/_shared/modules/date-picker/date-picker.module.ts
+++ src/app/_shared/modules/date-picker/date-picker.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DatePickerComponent} from './date-picker.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatDatepickerModule, MatFormFieldModule, MatNativeDateModule} from '@angular/material';
+import { MatDatepickerModule, MatFormFieldModule, MatIconModule, MatNativeDateModule } from '@angular/material';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
 
 @NgModule({
@@ -17,6 +17,7 @@ import {MatMomentDateModule} from '@angular/material-moment-adapter';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
+    MatIconModule,
   ],
   exports: [
     DatePickerComponent,
