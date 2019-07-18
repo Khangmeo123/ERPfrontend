2fdf36f728670d583ddde8bd35e9b390d0dfc184
Leanh1011 <leanh2194@gmail.com>
2019-07-18 15:08:35 +0700

    last up supplier-list-of-legal-entity

--- src/app/_modules/master-data/_modules/legal-entity/_page/supplier-of-legal-entity/list-supplier-of-legal-entity/list-supplier-of-legal-entity.component.ts
+++ src/app/_modules/master-data/_modules/legal-entity/_page/supplier-of-legal-entity/list-supplier-of-legal-entity/list-supplier-of-legal-entity.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TextFilter } from 'src/app/_shared/models/filters/TextFilter';
 import { Router } from '@angular/router';
+import { PaginationModel } from 'src/app/_shared/modules/pagination/pagination.model';
 @Component({
   selector: 'app-lisr-supplier-of-legal-entity',
   templateUrl: './list-supplier-of-legal-entity.component.html',
@@ -8,19 +9,61 @@ import { Router } from '@angular/router';
 })
 export class ListSupplierOfLegalEntityComponent implements OnInit {
 
+  pagination = new PaginationModel();
+
+  display: boolean = false;
+  isAddGroup = false;
+
+  isAddCustomer = false;
+
+  tmptable = [
+    {
+      code: 1,
+      name: 'Nguyễn Thị Hương',
+      customerGroup: 'huongnguyenhd96@gmail.com',
+      taxCode: 'hihi',
+      address: 1,
+      phone: '1000000',
+    },
+    {
+      code: 1,
+      name: 'Nguyễn Thị Hương',
+      customerGroup: 'huongnguyenhd96@gmail.com',
+      taxCode: 'hihi',
+      address: 1,
+      phone: '1000000',
+    },
+    {
+      code: 1,
+      name: 'Nguyễn Thị Hương',
+      customerGroup: 'huongnguyenhd96@gmail.com',
+      taxCode: 'hihi',
+      address: 1,
+      phone: '1000000',
+    },
+    {
+      code: 1,
+      name: 'Nguyễn Thị Hương',
+      customerGroup: 'huongnguyenhd96@gmail.com',
+      taxCode: 'hihi',
+      address: 1,
+      phone: '1000000',
+    },
+
+  ]
+
   constructor(protected router: Router) { }
-  filters = {
-    id: new TextFilter(),
-    name: new TextFilter(),
-    address: new TextFilter(),
-    tax_code: new TextFilter(),
-    phone: new TextFilter(),
-    status: new TextFilter()
-  }
+
 
   ngOnInit() {
   }
-  showdetail() {
-    this.router.navigate(['/master-data/legal-entity/supplier-of-legal-entity/detail-supplier-legal-entity']);
+
+
+  onClickShowDetail() {
+    this.router.navigate(['/master-data/legal-entity/customer-list-of-legal-entity/detail-customer-legal-entity']);
+  }
+
+  showDialog() {
+    this.display = true;
   }
 }
