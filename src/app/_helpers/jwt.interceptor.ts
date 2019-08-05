9caaea5da099efb815386923ce485f198edbd903
LeAnh <leanh2194@gmail.com>
2019-08-05 10:11:39 +0700

    fix run e2e

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -16,7 +16,7 @@ export class JwtInterceptor implements HttpInterceptor {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'X-BusinessGroup': 'AE0E3884-FB32-4DA1-9A51-BE6E80402A67',
-        Authentication: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI1N2JmMDE5OC1mMTA2LTQ1MGQtYmM1ZS1mMWY2NDUyZmRjNDciLCJ1bmlxdWVfbmFtZSI6Imh1eWJxIiwiQnVzaW5lc3NHcm91cElkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwiSXNTdXBlckFkbWluIjoiVHJ1ZSIsIm5iZiI6MTU2MzE1OTc0NiwiZXhwIjoxNTczMTU5NzQ1LCJpYXQiOjE1NjMxNTk3NDZ9.9ll68DtTqe0Rs43Pm7jHRJiYNuUCiEGnnC6trMAGt4Q',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI1N2JmMDE5OC1mMTA2LTQ1MGQtYmM1ZS1mMWY2NDUyZmRjNDciLCJ1bmlxdWVfbmFtZSI6Imh1eWJxIiwiQnVzaW5lc3NHcm91cElkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwiSXNTdXBlckFkbWluIjoiVHJ1ZSIsIm5iZiI6MTU2MzE1OTc0NiwiZXhwIjoxNTczMTU5NzQ1LCJpYXQiOjE1NjMxNTk3NDZ9.9ll68DtTqe0Rs43Pm7jHRJiYNuUCiEGnnC6trMAGt4Q',
       }),
     });
     return next.handle(request);
