ab25e5830ce3e97c866a93ccdc0a0bab7141c852
tungpt <thanhtung.uet@gmail.com>
2019-08-11 16:28:41 +0700

    Add authentication

--- src/app/_pages/login/login.component.ts
+++ src/app/_pages/login/login.component.ts
@@ -1,63 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../_services';
+import { GeneralService } from '../../_helpers/general-service.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [
+    GeneralService,
+  ],
 })
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-    loading = false;
-    submitted = false;
-    returnUrl: string;
-    error = '';
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) { 
-        // redirect to home if already logged in
-        if (this.authenticationService.currentUserValue) { 
-            this.router.navigate(['/']);
-        }
+  loading = false;
+
+  submitted = false;
+
+  returnUrl: string;
+
+  error = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private generalService: GeneralService,
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  get username() {
+    return this.loginForm.get('username') as FormControl;
+  }
+
+  get password() {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['thangld', Validators.required],
+      password: ['abc', Validators.required],
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      this.generalService.validateAllFormFields(this.loginForm);
     }
 
-    ngOnInit() {
-        this.loginForm = this.formBuilder.group({
-            username: ['', Validators.required],
-            password: ['', Validators.required]
+    if (this.loginForm.valid) {
+      this.authenticationService.login(this.username.value, this.password.value)
+        .then(() => {
+          this.router.navigate(['/']);
         });
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+      this.loading = true;
     }
-
-    // convenience getter for easy access to form fields
-    get f() { return this.loginForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.loginForm.invalid) {
-            return;
-        }
-
-        this.authenticationService.login(this.loginForm.controls.username.value, this.loginForm.controls.password.value)
-            .subscribe(res => {
-                if(res){
-                    this.router.navigate(['home'])
-                }
-        });
-
-        this.loading = true;
-    }
-
+  }
 }
--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,31 +1,60 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { UserEntity } from '../_entities/user/user.entity';
+import { AuthenticationRepository } from '../_repositories/authentication.repository';
+import { ToastrService } from 'ngx-toastr';
+import { translate } from '../_helpers/string';
+import { Router } from '@angular/router';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthenticationService {
-    private currentUserSubject: BehaviorSubject<any>;
-    public currentUser: Observable<any>;
+  public currentUser: Observable<any>;
 
-    constructor(private http: HttpClient) {
-        this.currentUserSubject = new BehaviorSubject<any>({});
-        this.currentUser = this.currentUserSubject.asObservable();
-    }
+  private currentUserSubject: BehaviorSubject<any>;
 
-    public get currentUserValue(): any {
-        return this.currentUserSubject.value;
+  constructor(
+    private http: HttpClient,
+    private authenticationRepository: AuthenticationRepository,
+    private toastrService: ToastrService,
+    private router: Router,
+  ) {
+    const user: UserEntity = JSON.parse(localStorage.getItem('currentUser')) || null;
+    if (user === null) {
+      this.currentUserSubject = new BehaviorSubject<any>(null);
+    } else {
+      this.currentUserSubject = new BehaviorSubject<any>(user);
     }
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-    login(username: string, password: string): Observable<any> {
-        return new Observable(observer => {
-            this.currentUserSubject.next('logged');
-            observer.next({ username: username, password: password })
-        })
-    }
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
 
-    logout() {
-        localStorage.removeItem('currentUser');
-        this.currentUserSubject.next(null);
-    }
+  login(username: string, password: string): Promise<UserEntity> {
+    return new Promise<UserEntity>((resolve, reject) => {
+      this.authenticationRepository.login(username, password)
+        .subscribe(
+          (user: UserEntity) => {
+            this.currentUserSubject.next(user);
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            resolve(user);
+          },
+          (error: Error) => {
+            reject(error);
+            this.toastrService.error(translate('login.failed'));
+          },
+        );
+    });
+  }
+
+  logout() {
+    this.currentUserSubject.next(null);
+    localStorage.removeItem('currentUser');
+    document.cookie = 'token=; expires=0';
+    return this.router.navigate(['login']);
+  }
 }
