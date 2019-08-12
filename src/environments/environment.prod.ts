a94457398f82562a29713dd107ee42d0a645a773
tungpt <thanhtung.uet@gmail.com>
2019-08-12 10:18:07 +0700

    Fix app url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlApps: 'http://app.jaja.vn/api/apps/',
+  apiUrlApps: 'http://app.jaja.vn/api/APPS/',
   emptyGuid: '00000000-0000-0000-0000-000000000000',
 };
