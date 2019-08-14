cb8915485a2c7f3d16538b7c54f7436916bb7750
Phạm Thanh Tùng <thanhtung.uet@gmail.com>
2019-08-14 14:48:45 +0700

    Update yarn.lock

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrlApps: 'http://app.jaja.vn/api/APPS/',
+  apiUrlApps: 'http://app.jaja.vn/api/apps/',
+  apiUrlInv: 'http://app.jaja.vn/api/inv/',
+  apiUrlAuthentication: 'http://app.jaja.vn/api/Authentication/',
   emptyGuid: '00000000-0000-0000-0000-000000000000',
 };
