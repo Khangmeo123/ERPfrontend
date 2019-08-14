a15bc6afe2371dc6f074dcbef19e3bd06a84874a
Phạm Thanh Tùng <thanhtung.uet@gmail.com>
2019-08-14 15:48:50 +0700

    Fix simple select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,16 @@
-import { Component } from '@angular/core';
-import { IdbService } from './_services/idb.service';
-import { CookieService } from 'ngx-cookie-service';
+import {Component} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [CookieService],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [CookieService],
 })
 export class AppComponent {
-  title = 'CodeBase';
+    title = 'JAJA';
 
-  constructor(private idbService: IdbService, private cookieService: CookieService) {
-    // this.idbService.connectToIDB().then(res => {
-    //   this.idbService.getAllKey('objectStoreValue').then(result => {
-    //     if (typeof Worker !== 'undefined') {
-    //       // Create a new
-    //       const worker = new Worker('./app.worker', { type: 'module' });
-    //       worker.onmessage = ({ data }) => {
-    //         console.log(data);
-    //       };
-    //       worker.postMessage({ name: 'NewDb', keys: result});
-    //     } else {
-    //       console.log('Browser not support Webworker');
-    //     }
-    //   });
-    // });
-  }
+    constructor() {
+
+    }
 }
