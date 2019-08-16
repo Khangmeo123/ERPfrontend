055b66a36ac0488d9e3b0a8f0699e3d9d4f90f68
tungpt <thanhtung.uet@gmail.com>
2019-08-16 19:26:22 +0700

    Fix case sensitive

--- src/app/_shared/modules/discussion/backend/discussion.searchentity.ts
+++ src/app/_shared/modules/discussion/backend/discussion.searchentity.ts
@@ -1,13 +1,13 @@
-import { CommentEntity } from './Comment.Entity';
-import { SearchEntity } from 'src/app/_helpers/search-entity';
+import { CommentEntity } from './comment.entity';
 
-export class SearchPostEntity{
-    id: string;
-    documentId: string;
-    content: string;
-    creatorId: string;
-    time: string;
-    comments: Array<CommentEntity>;
-    constructor(postSearchEntity: any = null) {
-    }
+export class SearchPostEntity {
+  id: string;
+  documentId: string;
+  content: string;
+  creatorId: string;
+  time: string;
+  comments: Array<CommentEntity>;
+
+  constructor(postSearchEntity: any = null) {
+  }
 }
