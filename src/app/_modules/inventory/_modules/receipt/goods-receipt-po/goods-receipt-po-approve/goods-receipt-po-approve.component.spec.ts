c0c224c7ebf6b1dee2cf65be5f9fcc5b94cdb959
tungpt <thanhtung.uet@gmail.com>
2019-08-23 09:16:36 +0700

    Fix goods receipt po approve

--- src/app/_modules/inventory/_modules/receipt/goods-receipt-po/goods-receipt-po-approve/goods-receipt-po-approve.component.spec.ts
+++ src/app/_modules/inventory/_modules/receipt/goods-receipt-po/goods-receipt-po-approve/goods-receipt-po-approve.component.spec.ts
@@ -1,25 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import { GoodsReceiptPOApproveComponent } from './goods-receipt-po-approve.component';
+import { GoodsReceiptPoDetailService } from './goods-receipt-po-detail.service';
 
-describe('GoodsIssueDetailComponent', () => {
-  let component: GoodsReceiptPOApproveComponent;
-  let fixture: ComponentFixture<GoodsReceiptPOApproveComponent>;
+describe('GoodsReceiptPoDetailService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [GoodsReceiptPOApproveComponent],
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GoodsReceiptPOApproveComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    const service: GoodsReceiptPoDetailService = TestBed.get(GoodsReceiptPoDetailService);
+    expect(service).toBeTruthy();
   });
 });
